bb889e9520cb456b8ee4a43802c75bc90a3702a3
Hanifan Pangabekti <gam2000e@gmail.com>
2025-07-06 14:29:33 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,61 @@
-//togle class active
-const navbarNav = document.querySelector('.navbar-nav');
+//togle class active for menu
+const navbarNav = document.querySelector(".navbar-nav");
 // ketika menu di klik
-document.querySelector('#menu')
-.onclick = () => {
-    navbarNav.classList.toggle('active');
-} ;
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+//togle class active for search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-//klik manapun, side bar akan hilang
-const menu = document.querySelector('#menu');
-document.addEventListener('click', function(e){
-    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+// togle class active for shopping-cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+    shoppingCart.classList.toggle("active");
+    e.preventDefault();
+}
+
+//klik manapun
+const menu = document.querySelector("#menu");
+const searchButton = document.querySelector("#search-button");
+const ShoppingButton = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!ShoppingButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
+// modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelectorAll(".item-detail-button");
+
+itemDetailButton.forEach((btn)=>{
+    btn.onclick = (e) => {
+        itemDetailModal.style.display="flex";
+        e.preventDefault();
+    }   
 })
+// klik tombol close
+document.querySelector(".close-icon").onclick = (e) => {
+    itemDetailModal.style.display="none";
+    e.preventDefault();
+}
+// klik di luar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = "none"; 
+    }
+}
